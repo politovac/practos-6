d2a8fe4dab1162171a03078bc0f5a98a22f9f0dc
ИСП-21 <-21@LAB44-06>
2022-10-14 15:56:10 +0300

    Добавьте файлы проекта.

---
+++ practos 6/Program.cs
@@ -0,0 +1,20 @@
+﻿//. Дано натуральное число. Определить: а) количество цифр 3 в нем; б) сколько
+//раз в нем встречается последняя цифра; в) количество четных цифр в нем. 
+int Kol = 0,
+value,dublicateValue;
+value = dublicateValue = Convert.ToInt32(Console.ReadLine());
+while ( dublicateValue != 0 )
+{
+    if (dublicateValue % 10 == 3)
+    {
+        Kol++;
+        dublicateValue /= 10;
+    }
+
+if ((dublicateValue % 10) % 2 == 0)
+ { 
+           Kol++;
+            dublicateValue = dublicateValue % 10;     
+ }
+}
+Console.WriteLine($"Кол-во цифр 3 в числе будет {value} будет {Kol}");
